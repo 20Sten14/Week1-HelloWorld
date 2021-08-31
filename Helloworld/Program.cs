72ded526e7e80c65f96aa85b0c24430f0374ab95
Opilane TTHK <opilane@tthk.ee>
2021-08-31 10:55:30 +0300

    add Hello World Code

---
+++ Helloworld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Tere Sten Tressum!");
+
+            Console.Read();
+        }
+
+    }
+}
